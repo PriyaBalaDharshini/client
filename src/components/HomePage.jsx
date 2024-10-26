4838c87042f3d0bce107565adc23f955bc7c4d54
PriyaBalaDharshini <priyaarasu12@gmail.com>
2024-10-26 12:58:54 +0530

    Hover effect added

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -9,36 +9,36 @@ const HomePage = () => {
                 <div className="items">
 
                     <div className="row-one flex items-center justify-around gap-5 mt-6">
-                        <div className="box-1  bg-gray-100 p-10 pb-[80px] rounded-l-3xl text-left">
+                        <div className="box-1  bg-gray-100 p-10 pb-[80px] rounded-l-3xl text-left hover:bg-gray-700 hover:text-white">
                             <h3 className='font-bold text-xl mb-5'> Extensive Knlowdge</h3>
                             <p>Lorem ipsum dolor sit amet consectetur.</p>
                         </div>
 
-                        <div className="box-1  bg-gray-100 p-10  pb-[80px] rounded-l-3xl text-left">
+                        <div className="box-1  bg-gray-100 p-10  pb-[80px] rounded-l-3xl text-left  hover:bg-gray-700 hover:text-white">
                             <h3 className='font-bold text-xl mb-5'> Extensive Knlowdge</h3>
                             <p>Lorem ipsum dolor sit amet consectetur.</p>
                         </div>
                     </div>
 
                     <div className="row-two flex items-center justify-evenly gap-5 mt-6">
-                        <div className="box-1  bg-gray-100 p-10 pb-[80px] rounded-l-3xl text-left">
+                        <div className="box-1  bg-gray-100 p-10 pb-[80px] rounded-l-3xl text-left  hover:bg-gray-700 hover:text-white">
                             <h3 className='font-bold text-xl mb-5'> Extensive Knlowdge</h3>
                             <p>Lorem ipsum dolor sit amet consectetur.</p>
                         </div>
                         <img src="./avatar.png" alt="" />
-                        <div className="box-1  bg-gray-100 p-10  pb-[80px] rounded-l-3xl text-left">
+                        <div className="box-1  bg-gray-100 p-10  pb-[80px] rounded-l-3xl text-left  hover:bg-gray-700 hover:text-white">
                             <h3 className='font-bold text-xl mb-5'> Extensive Knlowdge</h3>
                             <p>Lorem ipsum dolor sit amet consectetur.</p>
                         </div>
                     </div>
 
-                    <div className="row-one flex items-center justify-around gap-5 mt-6">
-                        <div className="box-1  bg-gray-100 p-10 pb-[80px] rounded-l-3xl text-left">
+                    <div className="row-three flex items-center justify-around gap-5 mt-6">
+                        <div className="box-1  bg-gray-100 p-10 pb-[80px] rounded-l-3xl text-left  hover:bg-gray-700 hover:text-white">
                             <h3 className='font-bold text-xl mb-5'> Extensive Knlowdge</h3>
                             <p>Lorem ipsum dolor sit amet consectetur.</p>
                         </div>
 
-                        <div className="box-1  bg-gray-100 p-10  pb-[80px] rounded-l-3xl text-left">
+                        <div className="box-1  bg-gray-100 p-10  pb-[80px] rounded-l-3xl text-left  hover:bg-gray-700 hover:text-white">
                             <h3 className='font-bold text-xl mb-5'> Extensive Knlowdge</h3>
                             <p>Lorem ipsum dolor sit amet consectetur.</p>
                         </div>
